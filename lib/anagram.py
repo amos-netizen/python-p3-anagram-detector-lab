9b4e05b8303555a2c40bf588eaea4f2a65beef69
amos <amosmokua12@gmail.com>
2024-09-08 13:09:01 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+
+class Anagram:
+  def __init__(self, word):
+    self.word = word
+  
+  def match(self,  word_list):
+    anagrams = []
+    for word in word_list:
+      if sorted(self.word) == sorted(word.lower()):
+        anagrams.append(word)
+    return anagrams
+
+
+  
